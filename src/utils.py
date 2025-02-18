823ad38582d4f9552b63e343c521b0847a1f4bcd
Pranav Kumar <pranav.1si22is067@gmail.com>
2025-02-19 01:01:35 +0530

    Changes Added

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+import math
+
+def calculate_distance(x1, y1, x2, y2):
+    """
+    Calculate the Euclidean distance between two points (x1, y1) and (x2, y2).
+    """
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+
+
